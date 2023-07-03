86efaec943e1855ed07e0838bd6eff95b3ad4d33
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2023-07-03 15:58:36 -0300

    modificacion de precios y whatsapp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,57 @@
-import React,{useState} from 'react';
-import {Link} from 'react-router-dom'
-import './NavbarStyles.css';
-import {FaBars,FaTimes} from 'react-icons/fa';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./NavbarStyles.css";
+import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "../assets/LogoMOVEMEcolor-02.png";
 
 function Navbar(props) {
-    const[click,setClick]=useState(false);
-    const handleClick = _=>setClick(!click);
-    const [color,setColor]=useState(false);
-    const changeColor=()=>{
-        if(window.scrollY >=100){
-            setColor(true);
-        }else{
-            setColor(false)
-        }
+  const [click, setClick] = useState(false);
+  const handleClick = (_) => setClick(!click);
+  const [color, setColor] = useState(false);
+  const changeColor = () => {
+    if (window.scrollY >= 100) {
+      setColor(true);
+    } else {
+      setColor(false);
     }
-window.addEventListener('scroll',changeColor);
+  };
+  window.addEventListener("scroll", changeColor);
 
-    return (
-        <div className={color ? 'header header-bg' : 'header'}>
-       <Link to='/' ><img src={logo} className='logo' alt="logo" width='10%' height='auto' /></Link>
-       <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+  return (
+    <div className={color ? "header header-bg" : "header"}>
+      <Link to="/">
+        <img
+          src={logo}
+          className="logo"
+          alt="logo"
+          width="7%"
+          padding="1rem"
+          height="auto"
+        />
+      </Link>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li>
-            <Link to="/">Inicio</Link>
+          <Link to="/">Inicio</Link>
         </li>
         <li>
-            <Link to="/pricing">Precios</Link>
+          <Link to="/pricing">Precios</Link>
         </li>
         <li>
-            <Link to="/training">Entrenamientos</Link>
+          <Link to="/training">Entrenamientos</Link>
         </li>
         <li>
-            <Link to="/contact">Contactame</Link>
+          <Link to="/contact">Contactame</Link>
         </li>
-
-       </ul>
-       <div className="hamburger" onClick={handleClick}>
-        {click ? (<FaTimes size={20} style={{color:'#fff'}}/>) : (<FaBars size={20} style={{color:'#fff'}}/>)}
-        
-
-       </div>
-            
-        </div>
-    );
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,63 +1,71 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-import './Pricing.css';
+import React from "react";
+import "./Pricing.css";
 
 function Pricing(props) {
-    return (
-        <div className='pricing'>
-            <div className="card-container">
-                <div className="card">
-                    <h3>6 Meses de entrenamiento Presencial</h3>
-                    <span className="bar"></span>
-                    <p className='btc'>$30.000</p>
-                    <p>- Corrección Postural -</p>
-                    <p>- Ayuda Permanente -</p>
-                    <p>- Seguimiento Continuo -</p>
-                    <p>- Control de Peso -</p>
-                    <p>- Horarios Flexibles -</p>
-                    <Link to='/contact' className='btn'>Comprar</Link>
-
-                </div>
-                <div className="card">
-                    <h3>12 Meses de entrenamiento Presencial</h3>
-                    <span className="bar"></span>
-                    <p className='btc'>$50.000</p>
-                    <p>- Corrección Postural -</p>
-                    <p>- Ayuda Permanente -</p>
-                    <p>- Seguimiento Continuo -</p>
-                    <p>- Control de Peso -</p>
-                    <p>- Horarios Flexibles -</p>
-                    <Link to='/contact' className='btn'>Comprar</Link>
-
-                </div>
-                <div className="card">
-                    <h3>6 Meses de entrenamiento Online</h3>
-                    <span className="bar"></span>
-                    <p className='btc'>$20.000</p>
-                    <p>- Corrección Postural -</p>
-                    <p>- Ayuda Permanente -</p>
-                    <p>- Seguimiento Continuo -</p>
-                    <p>- Control de Peso -</p>
-                    <p>- Horarios Flexibles -</p>
-                    <Link to='/contact' className='btn'>Comprar</Link>
-
-                </div>
-                <div className="card">
-                    <h3>6 Meses de entrenamiento Online</h3>
-                    <span className="bar"></span>
-                    <p className='btc'>$30.000</p>
-                    <p>- Corrección Postural -</p>
-                    <p>- Ayuda Permanente -</p>
-                    <p>- Seguimiento Continuo -</p>
-                    <p>- Control de Peso -</p>
-                    <p>- Horarios Flexibles -</p>
-                    <Link to='/contact' className='btn'>Comprar</Link>
-
-                </div>
-            </div>
-            
+  return (
+    <div className="pricing">
+      <div>
+        <h3 className="text-white">
+          Selecciona el tipo de entrenamiento que desees y nuestro Staff{" "}
+        </h3>
+      </div>
+      <div className="card-container">
+        <div className="card">
+          <h3>PLAN SÓLO RUTINA PERSONALIZADA</h3>
+          <span className="bar"></span>
+          <p className="btc">USD 12 mensual</p>
+          <p className="">Argentina: $4.500 tu tarjeta hace la conversión</p>
+          <p>✔ Rutina Personalizada </p>
+          <p>✔ Videos demonstrando los ejercicios </p>
+          <p>✔ Seguimiento por whastapp </p>
+          <p>✔ Ingreso a grupo privado </p>
+          <a
+            href="https://api.whatsapp.com/send?phone=541136710228&text=%C2%A1Hola%20Martin%20Ercolano!%20Quiero%20comprar%20el%20plan%20RUTINA%20PERSONALIZADA%20"
+            className="btn"
+          >
+            Quiero Empezar
+          </a>
         </div>
-    );
+        <div className="card">
+          <h3>PROGRAMA VIP + COMUNIDAD WHATSAPP</h3>
+          <span className="bar"></span>
+          <p className="btc">USD 25 mensual</p>
+          <p className="">Argentina: $9500 tu tarjeta hace la conversión</p>
+          <p>✔ Rutina Personalizada</p>
+          <p>✔ Plan de comidas Personalizado </p>
+          <p>✔ Lista de compras </p>
+          <p>✔ Diferentes opciones de comidas </p>
+          <p>✔ Videos demonstrando los ejercicios </p>
+          <p>✔ Grupo whatsapp motivación y coaching </p>
+          <a
+            href="https://api.whatsapp.com/send?phone=541136710228&text=%C2%A1Hola%20Martin%20Ercolano!%20Quiero%20comprar%20el%20plan%20PROGRAMA%20VIP%20"
+            className="btn"
+          >
+            Quiero Empezar
+          </a>
+        </div>
+        <div className="card">
+          <h3>PLAN PERSONALIZADO ELITE + COACHING</h3>
+          <span className="bar"></span>
+          <p className="btc">USD 150 mensual</p>
+          <p className="">Argentina: $57.000 tu tarjeta hace la conversión</p>
+          <p>✔ Rutina Personalizada </p>
+          <p>✔ Plan de comidas Personalizado </p>
+          <p>✔ Lista de compras </p>
+          <p>✔ Diferentes opciones de comidas </p>
+          <p>✔ Videos demonstrando los ejercicios </p>
+          <p>✔ Grupo whatsapp motivación y coaching </p>
+          <p>✔ Video llamada privada de coaching 1 a 1 </p>
+          <a
+            href="https://api.whatsapp.com/send?phone=541136710228&text=%C2%A1Hola%20Martin%20Ercolano!%20Quiero%20comprar%20el%20plan%20ELITE%20COACHING%20"
+            className="btn"
+          >
+            Quiero Empezar
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
